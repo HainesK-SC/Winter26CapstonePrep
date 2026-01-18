f4b4dfd32cac356a430e10896316708a708f7d01
HainesK-SC <hainesk@sheridancollege.ca>
2026-01-18 14:27:06 -0500

    Adding SpringBoot project skeleton to the repo

---
+++ app/CapstonePrepProject/src/main/java/ca/sheridancollege/hainesk/capstoneprepproject/CapstonePrepProjectApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.hainesk.capstoneprepproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstonePrepProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstonePrepProjectApplication.class, args);
+	}
+
+}
---
+++ app/CapstonePrepProject/src/test/java/ca/sheridancollege/hainesk/capstoneprepproject/CapstonePrepProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.hainesk.capstoneprepproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstonePrepProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
